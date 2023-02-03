e09379ebcdd5286b87579c0f026778f76efbb146
Martin Stewart <m.stewart796@gmail.com>
2023-02-03 15:19:32 -0300

    Add print to fourth exercise to show results

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,10 +1,10 @@
 public class BarkingDog {
     public static void main(String[] args) {
 
-        shouldWakeUp (true, 1);  // → should return true
-        shouldWakeUp (false, 2);  // → should return false since the dog is not barking.
-        shouldWakeUp (true, 8);  // → should return false, since it's not before 8.
-        shouldWakeUp (true, -1);  // → should return false since the hourOfDay parameter needs to be in a range 0-23.
+        System.out.println(shouldWakeUp (true, 1));  // → should return true
+        System.out.println(shouldWakeUp (false, 2));  // → should return false since the dog is not barking.
+        System.out.println(shouldWakeUp (true, 8));  // → should return false, since it's not before 8.
+        System.out.println(shouldWakeUp (true, -1));  // → should return false since the hourOfDay parameter needs to be in a range 0-23.
 
     }
 
