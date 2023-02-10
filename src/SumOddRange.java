b487099d864b0e97b6cc7bc8a5e21a8f6429ddce
Martin Stewart <m.stewart796@gmail.com>
2023-02-09 21:16:13 -0300

    Completed exercise 15, SumOddRange

--- src/SumOddRange.java
+++ src/SumOddRange.java
@@ -1,4 +1,48 @@
 public class SumOddRange {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOdd(1, 100));  // → should return 2500
+        System.out.println(sumOdd(-1, 100));  // →  should return -1
+        System.out.println(sumOdd(100, 100));  // → should return 0
+        System.out.println(sumOdd(13, 13));  // → should return 13 (This set contains one number, 13, and it is odd)
+        System.out.println(sumOdd(100, -100));  // → should return -1
+        System.out.println(sumOdd(100, 1000));  // → should return 247500
+
+    }
+
+    public static boolean isOdd(int number) {
+
+        if (number <= 0) {
+            return false;
+        }
+
+        if ((number % 2) != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end) {
+
+        int totalSum = 0;
+
+        if (end < start) {
+            return -1;
+        }
+
+        if (start < 0 || end < 0) {
+            return -1;
+        }
+
+        for (int i = start; i <= end; i++ )
+            if (isOdd(i)) {
+                totalSum += i;
+            }
+
+        return totalSum;
+    }
 }
 //    Write a method called isOdd with an int parameter and call it number. The method needs to return a boolean.
 //
