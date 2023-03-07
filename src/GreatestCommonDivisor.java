4975902eafd19086ea47b298ea27a8e65c64b19c
Martin Stewart <m.stewart796@gmail.com>
2023-03-06 22:23:46 -0300

    Set up tests ex. 22, GreatestCommonDivisor

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,10 +1,20 @@
 public class GreatestCommonDivisor {
     public static void main(String[] args) {
 
+        getGreatestCommonDivisor(25, 15);  // should return 5 since both can be divided by 5 without a remainder
+        getGreatestCommonDivisor(12, 30);  // should return 6 since both can be divided by 6 without a remainder
+        getGreatestCommonDivisor(9, 18);  // should return -1 since the first parameter is < 10
+        getGreatestCommonDivisor(81, 153);  // should return 9 since both can be divided by 9 without a remainder
+
     }
 
     public static int getGreatestCommonDivisor(int first, int second) {
 
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        return 0;
     }
 }
 
