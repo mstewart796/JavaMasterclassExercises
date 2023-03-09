58aa26b4a87874cb0d5c26498cef088bc82d51df
Martin Stewart <m.stewart796@gmail.com>
2023-03-09 14:13:27 -0300

    Completed first check ex. 26, LargestPrime

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -1,16 +1,21 @@
 public class LargestPrime {
     public static void main(String[] args) {
 
-        System.out.println(getLargestPrime (21));
-        System.out.println(getLargestPrime (217));
-        System.out.println(getLargestPrime (0));
-        System.out.println(getLargestPrime (45));
-        System.out.println(getLargestPrime (-1));
+        System.out.println(getLargestPrime (21));  // should return 7 since 7 is the largest prime (3 * 7 = 21)
+        System.out.println(getLargestPrime (217)); // should return 31 since 31 is the largest prime (7 * 31
+        // = 217)
+        System.out.println(getLargestPrime (0));  // should return -1 since 0 does not have any prime numbers
+        System.out.println(getLargestPrime (45));  // should return 5 since 5 is the largest prime (3 * 3 * 5
+        // = 45)
+        System.out.println(getLargestPrime (-1)); // should return -1 since the parameter is negative
 
     }
 
     public static int getLargestPrime(int number) {
 
+        if (number < 0 || number % 2 == 0) {
+            return -1;
+        }
         return 0;
     }
 }
