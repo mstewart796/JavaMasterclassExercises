8aef87e0a68bd96453f9446133dfcbd0398e83ff
Martin Stewart <m.stewart796@gmail.com>
2023-03-09 13:32:24 -0300

    Implemented check for negatives in canPack method in ex. 25, FlourPacker

--- src/FlourPacker.java
+++ src/FlourPacker.java
@@ -15,6 +15,10 @@ public class FlourPacker {
 
     public static boolean canPack(int bigCount, int smallCount, int goal) {
 
+        // check if any parameter is negative
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
         return true;
     }
 }
