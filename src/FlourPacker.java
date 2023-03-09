c2ddcd74cf10c0f7cd1090a0186ca153d844c082
Martin Stewart <m.stewart796@gmail.com>
2023-03-09 13:49:34 -0300

    Completed ex. 25, FlourPacker

--- src/FlourPacker.java
+++ src/FlourPacker.java
@@ -1,25 +1,37 @@
 public class FlourPacker {
     public static void main(String[] args) {
 
-        System.out.println(canPack (1, 0, 4));  // should return false since bigCount is 1 (big bag of 5 kilos)
-        // and goal is 4 kilos
-        System.out.println(canPack (1, 0, 5));  // should return true since bigCount is 1 (big bag of 5 kilos) and
-        // goal is 5 kilos.
-        System.out.println(canPack (0, 5, 4));  // should return true since smallCount is 5 (small bags of 1 kilo) and
-        // goal is 4 kilos, and we have 1 bag left which is ok as mentioned above.
-        System.out.println(canPack (2, 2, 11));  // should return true since bigCount is 2 (big bags 5 kilos each)
-        // and smallCount is 2 (small bags of 1 kilo), makes in total 12 kilos and goal is 11 kilos.
-        System.out.println(canPack (-3, 2, 12));  // should return false since bigCount is negative.
+        System.out.println(canPack (1, 0, 4));  // should return false since bigCount is 1
+        // (big bag of 5 kilos) and goal is 4 kilos
+        System.out.println(canPack (1, 0, 5));  // should return true since bigCount is 1
+        // (big bag of 5 kilos) and goal is 5 kilos.
+        System.out.println(canPack (0, 5, 4));  // should return true since smallCount is 5
+        // (small bags of 1 kilo) and goal is 4 kilos, and we have 1 bag left which is ok as mentioned above.
+        System.out.println(canPack (2, 2, 11));  // should return true since bigCount is 2
+        // (big bags 5 kilos each) and smallCount is 2 (small bags of 1 kilo), makes in total 12 kilos and goal is
+        // 11 kilos.
+        System.out.println(canPack (-3, 2, 12));  // should return false since bigCount
+        // is negative.
 
     }
 
     public static boolean canPack(int bigCount, int smallCount, int goal) {
-
         // check if any parameter is negative
         if (bigCount < 0 || smallCount < 0 || goal < 0) {
             return false;
         }
-        return true;
+
+        int bigFlour = bigCount * 5;
+        // check if total is less than goal
+        if (bigFlour + smallCount < goal) {
+            return false;
+        }
+
+        if (goal % 5 <= smallCount) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
